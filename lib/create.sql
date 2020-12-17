270d5391da03c4833a56e8abc1d5e40cb8049821
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-17 19:27:59 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,17 +5,17 @@ CREATE TABLE projects (
   funding_goal NUMBER,
   start_date DATE,
   end_date DATE
-)
+);
 
 CREATE TABLE users (
   id INTEGER PRIMARY KEY,
   name TEXT,
   age INTEGER
-)
+);
 
 CREATE TABLE pledges (
   id INTEGER PRIMARY KEY,
   user_id INTEGER FOREIGN KEY,
   project_id INTEGER FOREIGN KEY,
   amount NUMBER
-)
+);
